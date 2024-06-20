57e502851bf578fac64feb79e3eeb955dd04116c
hinamuriayu13 <harukanyan1@gmail.com>
2024-06-20 10:16:28 +0000

    mycomm

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # to Fix wrong 'phpp' file extension in the file 'wp-settings.php'
 
-exec{'fix-wordpress':
- command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
- path    => '/usr/local/bin/:/bin/'
+exec { 'fix-wordpress':
+  command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
+  path    => ['/usr/local/bin', '/bin'],
 }
